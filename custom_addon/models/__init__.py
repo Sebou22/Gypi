b68a1f2f6d9c531116b848c63dd262113e499463
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2023-03-22 13:02:35 +0000

    Add traduction

--- custom_addon/models/__init__.py
+++ custom_addon/models/__init__.py
@@ -1,6 +1,6 @@
 from . import product_template_inherit
 from . import product_public_category_inherit
-#from . import website_inherit
+from . import website_inherit
 from . import sale_order_line_inherit
 from . import sale_order_inherit
 from . import stock_picking_inherit
--- custom_addon/models/website_inherit.py
+++ custom_addon/models/website_inherit.py
@@ -6,5 +6,4 @@ class WebsiteInherit(models.Model):
 
     def sale_product_domain(self):
         return ['&'] + super(WebsiteInherit, self).sale_product_domain() + [
-            ('id', 'not in', self.env.user.product_ids.ids),
-            ('public_categ_ids', 'not in', self.env.user.category_ids.ids)]
+            ('restrict_ok', '=', True)]
