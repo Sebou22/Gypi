7fa0a2e86e3d06b671e9eddd1157c9fda2c40bdb
Youness <asmaa.khallout@dkgroup.fr>
2023-03-31 21:22:43 +0000

    Add location bar code in preparation order

--- gypi_picking_order/models/product_list.py
+++ gypi_picking_order/models/product_list.py
@@ -9,4 +9,12 @@ class ProductList(models.Model):
     product_uom_qty = fields.Float('Demand',digits='Product Unit of Measure',default=0.0)
     reserved_availability = fields.Float('Quantity Reserved',digits='Product Unit of Measure',)
     preparation_id = fields.Many2one('preparation.order', 'Preparation', index=True, check_company=True)
+    location_barcode = fields.Char("Locations",compute="_get_location_barcode")
 
+
+    def _get_location_barcode(self):
+        for record in self:
+            stock = self.env["stock.quant"].search([('product_id','=',record.product_id.id),('quantity','>',0)])
+            if(stock):
+                location_ids = stock.mapped('location_id').filtered(lambda r : r.barcode)
+                record.location_barcode = ','.join(l.barcode  for l in location_ids)
