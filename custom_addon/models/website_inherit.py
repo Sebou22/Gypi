4bcb227c8868013fe95237869cb519ad3a535858
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2023-04-17 14:18:06 +0000

    Update arch

--- custom_addon/models/website_inherit.py
+++ custom_addon/models/website_inherit.py
@@ -7,4 +7,4 @@ class WebsiteInherit(models.Model):
     def sale_product_domain(self):
         return ['&'] + super(WebsiteInherit, self).sale_product_domain() + [
             ('id', 'not in', self.env.user.partner_id.product_ids.ids),
-            ('public_categ_ids', 'not in', self.env.user.partner_id.category_ids.ids)]
+            ('public_categ_ids.restriction_type', '=', 'visible')]
