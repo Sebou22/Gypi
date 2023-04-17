1c439c606f7583180596b88dd24b394a99a3d90c
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2023-04-17 14:32:00 +0000

    Update arch

--- custom_addon/models/website_inherit.py
+++ custom_addon/models/website_inherit.py
@@ -5,6 +5,13 @@ class WebsiteInherit(models.Model):
     _inherit = 'website'
 
     def sale_product_domain(self):
+        categories = self.env['product.public.category'].search([('restriction_type','=','neither')])
+        list_ids =[]
+        partner_id = self.env.user.partner_id
+        for cat in categories:
+            if cat.id not in partner_id.category_ids.ids:
+                list_ids.append(cat.id)
+
         return ['&'] + super(WebsiteInherit, self).sale_product_domain() + [
             ('id', 'not in', self.env.user.partner_id.product_ids.ids),
-            ('public_categ_ids.restriction_type', '=', 'visible')]
+            ('public_categ_ids', 'not in', list_ids)]
