ab2b0f885f7ac5ec06f61b671b2da3a818a1c517
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2023-04-18 10:11:20 +0000

    Update arch

--- custom_addon/models/res_partner_inherit.py
+++ custom_addon/models/res_partner_inherit.py
@@ -14,11 +14,11 @@ class ResPartnerInherit(models.Model):
     category_ids = fields.Many2many('product.public.category', string='Category')
     product_ids = fields.Many2many('product.template', string='Products')
 
-    # def write(self, values):
-    #     res = super(ResPartnerInherit, self).write(values)
-    #     if 'category_ids' in values or 'product_ids' in values:
-    #         self.clear_caches()
-    #     return res
+    def write(self, values):
+        res = super(ResPartnerInherit, self).write(values)
+        if 'category_ids' in values or 'product_ids' in values:
+            self.clear_caches()
+        return res
 
     def get_restriction_categ(self):
         categories = self.env['product.public.category'].search([('restriction_type','=','all')])
