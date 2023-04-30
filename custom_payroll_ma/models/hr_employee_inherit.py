c8eb9f1efc72dfe7a986fd5a8e06bd32c0f8bb7f
Asmaa-khallout <83708600+Asmaa-khallout@users.noreply.github.com>
2023-04-30 23:21:55 +0100

    Update hr_employee_inherit.py

--- custom_payroll_ma/models/hr_employee_inherit.py
+++ custom_payroll_ma/models/hr_employee_inherit.py
@@ -6,3 +6,6 @@ from odoo import api, fields, models,exceptions, _
 class HrEmployeeInherit(models.Model):
     _inherit = 'hr.employee'
     loans_ids = fields.One2many('hr.loans.line','employee_id')
+    ifu = fields.Char(string='IFU')
+    n_ppr = fields.Char(string='PPR')
+    otherid = fields.Char('NumCE')
