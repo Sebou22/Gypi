c873b2f71c3fc92bf6951a1715ed149f1383d096
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2023-05-05 09:28:50 +0100

    Addd addon

--- report_custom_vindi/__manifest__.py
+++ report_custom_vindi/__manifest__.py
@@ -4,7 +4,7 @@
     'author': 'KHALLOUT Asmaa',
     "license": "AGPL-3",
     "version": "16.0.1",
-    'depends': ['stock'],
+    'depends': ['stock','sale'],
     'data': [
         'security/ir.model.access.csv',
         'views/product_product_views_inherit.xml',
