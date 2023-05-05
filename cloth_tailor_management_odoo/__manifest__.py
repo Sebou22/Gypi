92c89228eb4892884a8ac6a34c452da44c758461
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2023-05-05 15:38:52 +0100

    Update __manifest__.py

--- cloth_tailor_management_odoo/__manifest__.py
+++ cloth_tailor_management_odoo/__manifest__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
-
 {
     'name' : 'Tailor Management (Clothes)',
     'version' : '8.1.3',
